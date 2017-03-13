75290eb2d1ceecb549621b4d6e631fa846dd9b4c
Jonathan Wagner <jonathan.wagner@telecom-paristech.fr>
2017-03-13 18:39:55 +0100

    Application is launching the gameview

--- src/snake_game/Application.java
+++ src/snake_game/Application.java
@@ -1,17 +1,64 @@
 package snake_game;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 /**
  * Created by martin on 13.03.17.
  */
-public class Application {
-    public static Application instance = null;
+
+public class Application extends StateBasedGame {
+
+    private static Application instance = null;
+    private static AppGameContainer appContainer = null;
+    private static GameViewer gameView = null;
+
+    // Application Properties
+    private static final int WIDTH   = 640;
+    private static final int HEIGHT  = 480;
+    private static final int FPS     = 60;
+    private static final double VERSION = 1.0;
+
 
     public static Application getApp() {
         return instance;
     }
+    public static AppGameContainer getAppContainer() {
+        return appContainer;
+    }
 
-    public static void main(){
-    //    instance = new Application();
-        
+    public Application(String appName){
+        super(appName);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        gameView = new GameViewer(1);
+        this.addState(gameView);
+        this.enterState(gameView.getID());
+    }
+
+    public static void main(String[] args){
+        try {
+            instance = new Application("Potato Snake v" + VERSION);
+            appContainer = new AppGameContainer(instance);
+            appContainer.setDisplayMode(WIDTH, HEIGHT, false);
+            appContainer.setTargetFrameRate(FPS);
+            appContainer.setShowFPS(true);
+            appContainer.start();
+        } catch(SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getWIDTH() {
+        return WIDTH;
+    }
+
+
+    public static int getHEIGHT() {
+        return HEIGHT;
     }
 }
