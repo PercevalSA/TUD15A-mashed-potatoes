85f296f4f4b8357860d5453be4d32b254a3a6512
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2017-03-14 12:05:06 +0100

    minor changes

--- src/snake_game/GameViewer.java
+++ src/snake_game/GameViewer.java
@@ -35,9 +35,7 @@ public class GameViewer extends BasicGameState{
 
         SnakeHead snake_head = app.getSnakeHead();
         ArrayDeque<SnakeBody> snakeArray = app.getSnakeArray();
-
         Food food = app.getFood();
-
 
         g.setColor(Color.blue);
         for (SnakeBody element : snakeArray) {
@@ -45,19 +43,16 @@ public class GameViewer extends BasicGameState{
             float body_y = element.getY();
 
 
-            Rectangle rectangle_shape = new Rectangle(body_x,body_y,Application.getITEMSIZE(),Application.getITEMSIZE());
-
-            g.draw(rectangle_shape);
-            g.fill(rectangle_shape);
+            Rectangle body_shape = new Rectangle(body_x,body_y,Application.getITEMSIZE(),Application.getITEMSIZE());
+            g.draw(body_shape);
+            g.fill(body_shape);
         }
 
-
-
-        Rectangle rectangle_shape = new Rectangle(snake_head.getX(),snake_head.getY(),Application.getITEMSIZE(),Application.getITEMSIZE());
+        Rectangle head_shape = new Rectangle(snake_head.getX(),snake_head.getY(),Application.getITEMSIZE(),Application.getITEMSIZE());
 
         g.setColor(Color.white);
-        g.draw(rectangle_shape);
-        g.fill(rectangle_shape);
+        g.draw(head_shape);
+        g.fill(head_shape);
 
         Rectangle wall = new Rectangle(1,1,app.getWIDTH()-1,app.getGAMEHEIGHT()-1);
         g.setColor(Color.white);
