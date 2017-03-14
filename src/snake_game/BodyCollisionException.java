a99affd0857cc2cca85a14a448693e49d4cd5121
Jonathan Wagner <jonathan.wagner@telecom-paristech.fr>
2017-03-14 12:28:18 +0100

    Add gameover screen

--- src/snake_game/BodyCollisionException.java
+++ src/snake_game/BodyCollisionException.java
@@ -4,4 +4,11 @@ package snake_game;
  * Created by tsepol on 14-03-2017.
  */
 public class BodyCollisionException extends Exception {
+    public BodyCollisionException(){
+        super();
+        System.out.println("You ate yourself");
+        Application app = Application.getApp();
+        app.getMainMenu().setFirstTimeLaunched();
+        app.enterState(app.getMainMenu().getID());
+    }
 }
--- src/snake_game/WallCollisionException.java
+++ src/snake_game/WallCollisionException.java
@@ -6,6 +6,7 @@ public class WallCollisionException extends Exception {
         super();
         System.out.println("You collided with a wall");
         Application app = Application.getApp();
+        app.getMainMenu().setFirstTimeLaunched();
         app.enterState(app.getMainMenu().getID());
     }
 }
