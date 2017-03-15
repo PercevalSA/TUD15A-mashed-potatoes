29feae8c7c3d5862c1488e6717ff33506bedd078
Martin Sundermeyer <msundermeyer@gmx.de>
2017-03-15 16:40:21 +0100

    politicians

--- src/snake_game/Food.java
+++ src/snake_game/Food.java
@@ -3,14 +3,18 @@ package snake_game;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 
 import java.util.ArrayDeque;
+import java.util.Random;
 
 
 public class Food extends Item {
 
     private TasteBehavior tasteBehavior;
+    private float random_num;
 
     public Food(float x, float y, boolean isGood){
         super(x,y, null);
@@ -20,7 +24,10 @@ public class Food extends Item {
         }
         else{
             color = Color.red;
+            Random rand = new Random();
+            random_num = rand.nextFloat();
             tasteBehavior = new TasteBad();
+
         }
     }
 
@@ -34,4 +41,31 @@ public class Food extends Item {
             snakeArray.removeLast();
         }
     }
+
+    public void drawItem(Graphics g){
+
+
+        if (tasteBehavior.eat() == -1){
+            Image img = null;
+
+            try {
+                if (random_num>0.66f) {
+                    img = new Image("res/GeertWilders2.jpeg");
+                }else if(random_num<0.33f){
+                    img = new Image("res/marinelepen.jpg");
+                }else{
+                    img = new Image("res/FraukePetry.jpg");
+                }
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+            img.draw(x_position, y_position,Application.ITEMSIZE,Application.ITEMSIZE);
+        }else{
+            super.drawItem(g);
+        }
+
+
+
+    }
+
 }
